64dde4f89db0f2e50057e101644d1f02a84ef786
Arthur L Molina <arthurlavagnollimolina@gmail.com>
2026-03-18 20:01:12 -0300

    Initial commit

---
+++ src/main/java/com/example/gerenciamento_de_clientes/GerenciamentoDeClientesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_de_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeClientesApplication.class, args);
+	}
+
+}
