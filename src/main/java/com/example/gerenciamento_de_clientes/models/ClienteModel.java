8a9a8ddf4694c44c9722b0614d7684d0a576799a
Arthur L Molina <arthurlavagnollimolina@gmail.com>
2026-03-23 17:34:48 -0300

    model criado

---
+++ src/main/java/com/example/gerenciamento_de_clientes/models/ClienteModel.java
@@ -0,0 +1,55 @@
+package com.example.gerenciamento_de_clientes.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cliente ")
+public class ClienteModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nomeCliente")
+    private String nome;
+
+    @Column(name = "emailCliente")
+    private String email;
+
+    private String telefone;
+
+    public ClienteModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
