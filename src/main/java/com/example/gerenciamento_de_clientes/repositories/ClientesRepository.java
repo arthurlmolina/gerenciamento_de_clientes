dd82610af1961952b1a62e24db2d6fb46ab2f7a1
Arthur L Molina <arthurlavagnollimolina@gmail.com>
2026-03-23 17:39:55 -0300

    repository criado

---
+++ src/main/java/com/example/gerenciamento_de_clientes/repositories/ClientesRepository.java
@@ -0,0 +1,7 @@
+package com.example.gerenciamento_de_clientes.repositories;
+
+import com.example.gerenciamento_de_clientes.models.ClienteModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientesRepository extends JpaRepository<ClienteModel, Long> {
+}
