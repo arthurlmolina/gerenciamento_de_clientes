3427d4dd909307e77133935f0be9b198936b7282
Arthur L Molina <arthurlavagnollimolina@gmail.com>
2026-03-24 16:47:11 -0300

    controller criado

---
+++ src/main/java/com/example/gerenciamento_de_clientes/controllers/ClienteController.java
@@ -0,0 +1,49 @@
+package com.example.gerenciamento_de_clientes.controllers;
+
+import com.example.gerenciamento_de_clientes.models.ClienteModel;
+import com.example.gerenciamento_de_clientes.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/clientes")
+public class ClienteController {
+
+    @Autowired
+    ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<List<ClienteModel>> findAll(){
+        List<ClienteModel> request = clienteService.findAll();
+        return ResponseEntity.ok().body(request);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<ClienteModel> buscarPorId(@PathVariable Long id){
+        return clienteService.buscarPorId(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClienteModel> criar(@RequestBody ClienteModel clienteModel){
+        ClienteModel request = clienteService.criar(clienteModel);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(clienteModel.getId()).toUri();
+        return ResponseEntity.created(uri).body(request);
+    }
+
+    @PutMapping("/{id}")
+    public ClienteModel atualizar(@RequestBody ClienteModel clienteModel, @PathVariable Long id){
+        return clienteService.atualizar(clienteModel,id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletar(@PathVariable Long id){
+        clienteService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
